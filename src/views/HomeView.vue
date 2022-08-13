fb8d2341bc8ef21373d62da2a069120d4e2c2e9e
SongArtish <bulgen@naver.com>
2022-08-13 13:13:26 +0900

    initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="Home">
+    <h1>This is an home page</h1>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'HomeView',
+  components: {
+  }
+}
+</script>
+<style scoped>
+
+</style>
