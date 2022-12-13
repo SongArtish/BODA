70453c5a64eee50d575b5baf61b659b72cb1b445
drjsdud7 <drjsdud7@gmail.com>
2022-12-13 12:49:19 +0900

    상단이동 버튼, 헤더고정 추가

--- src/components/atoms/NavBar.vue
+++ src/components/atoms/NavBar.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="navbar">
-    <div class="navbar-title">찬양공유 <span v-if="isAdmin">관리자시스템</span></div>
-    <div class="navbar-button" v-if="isAdmin">
-      <button class="navbar-button-logout" @click="logout"><small>로그아웃</small></button>
+  <div class="wrapper">
+    <div class="navbar">
+      <div class="navbar-title">찬양공유 <span v-if="isAdmin">관리자시스템</span></div>
+      <div class="navbar-button" v-if="isAdmin">
+        <button class="navbar-button-logout" @click="logout"><small>로그아웃</small></button>
+      </div>
     </div>
+    <div class="navbar-area"></div>
   </div>
 </template>
 <script>
@@ -26,17 +29,35 @@ export default {
 }
 </script>
 <style scoped>
+.wrapper {
+  margin-bottom: 35px;
+}
+
 .navbar {
-  margin: 3rem 2rem;
+  position: fixed;
+
+  height: 60px;
+  width: 100%;
+  background: var(--color-bg);
+  z-index: 4;
+}
+
+.navbar-title{
+  height: 20px;
+  margin: 0 24px;
+}
+
+.navbar-area {
+  height: 60px;
 }
 
 .navbar-button-logout {
   background: #6E707F;
   border: 0;
   border-radius: .3rem;
+  margin: 0 24px;
   color: white;
 }
-
 
 .navbar-button-logout {
   background: #6E707F;
